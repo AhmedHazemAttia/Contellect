a6d7bfb15c0497a1adb1205c68f0ca299f070523
Ahmed <Ahmed_hazem.atia@hotmail.com>
2025-05-23 21:55:07 +0300

    (FIX) CORS Error

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,17 +6,22 @@ import http from 'http';
 import { Server } from 'socket.io';
 import contactRoutes from './routes/contactRoutes';
 import authRoutes from './routes/authRoutes';
-
+import cors from 'cors';
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || '';
 app.use(express.json());
+
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
+
 app.use('/api/contacts', contactRoutes);
 app.use('/api/auth', authRoutes);
-
 const io = new Server(server, {
-  cors: { origin: 'http://localhost:4200' } 
+  cors: { origin: '*' } 
 });
 const editLocks = new Map<string, string>();
 
